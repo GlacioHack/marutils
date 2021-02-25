52683dc2fa963f85b05f0936cde3953237c1bc6a
Andrew Tedstone <andrew.tedstone@unifr.ch>
2021-02-25 12:52:58 +0100

    change over to using setup.cfg

--- setup.py
+++ setup.py
@@ -4,17 +4,7 @@ from os import path
 FULLVERSION = '1.0.0'
 VERSION = FULLVERSION
 
-setup(name='marutils',
-      version=FULLVERSION,
-      description='Utilities and tools for working with MAR regional climate model outputs through xarray',
-      url='https://www.github.com/GlacioHack/marutils/',
-      author='Andrew Tedstone',
-      license='BSD-3',
-      packages=['marutils'],
-      install_requires=['rioxarray', 'xarray', 'pandas', 'rasterio'],
-      #extras_require=[],
-      scripts=[],
-      zip_safe=False)
+setup()
 
 write_version = True
 
